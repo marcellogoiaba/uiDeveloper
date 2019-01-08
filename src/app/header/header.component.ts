5c5b1b5072b1a0f0751be57ac5909bf5f02ebded
Marcelo Silva <msilva@iris.cat>
2019-01-08 11:42:14 +0000

    added collapse menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare let $: any;
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public showCollapseMenu: boolean = false;
+  public imageLink: string = '../../assets/icons/menu-simple-line-icons@2x.png';
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  showHideMenu(){
+    this.showCollapseMenu === false ? 
+    (this.showCollapseMenu = true, this.imageLink =  '../../assets/icons/close-simple-line-icons@2x.png'): 
+    (this.showCollapseMenu = false, this.imageLink = '../../assets/icons/menu-simple-line-icons@2x.png')
   }
 
 }
